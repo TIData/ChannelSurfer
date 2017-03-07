e5cfc8757c57dec05d5ef7d0df2cc0efecc925ec
Sebastiaan Fernandez <sebastiaan.fernandez.w1944@student.hogent.be>
2017-03-07 15:13:29 +0100

    Add initial commit

---
+++ src/oefeningiteratorstarter/OefeningIteratorStarter.java
@@ -0,0 +1,13 @@
+package oefeningiteratorstarter;
+
+import domein.ChannelSurfer;
+import domein.Iterator;
+import domein.Television;
+
+public class OefeningIteratorStarter {
+
+    public static void main(String[] args) {
+        Television television = new Television(3);
+    }
+    
+}
